81794dcdafbceb28e6897f3257287f623067737b
Pablo Hunter <aceleradora@MacBook-Pro-do-Pablo.local>
2023-11-08 15:10:03 -0300

    my firstcommit

--- node-js/index.js
+++ node-js/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// 1 euro = 5,23 reais
+
+
+
+const current = (value, coin) => {
+    let exchangeRate = {
+        EUR: 0.19,
+        AUD: 0.32,
+        USD: 0.20
+    }
+
+    console.log(`${coin} ${ (value * exchangeRate[coin]).toFixed(2)} BRL${value}`)
+}
+
+current(20, 'AUD')
